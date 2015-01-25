6b55b9d2516b537dcbebe9e21edaa28e324d6e07
Sasamus <labus93@gmail.com>
2015-01-25 13:56:39 +0100

    Implemented class LinearGuesser

--- src/se/miun/dt015a/guesser/LinearGuesser.java
+++ src/se/miun/dt015a/guesser/LinearGuesser.java
@@ -1,5 +1,7 @@
 package se.miun.dt015a.guesser;
 
+import se.miun.dt015a.guesser.Oracle.Answer;
+
 /**
  * Implements a linear guessing algorithm.
  * Complete this implementation so that findNumber returns the secret number
@@ -12,10 +14,32 @@ package se.miun.dt015a.guesser;
  * @author Christoffer Fink
  */
 public class LinearGuesser implements Guesser {
-  // Do not add any instance variables or constructors.
+	// Do not add any instance variables or constructors.
 
-  @Override
-  public int findNumber(int max, Oracle oracle) {
-    return -1; // Your code here.
-  }
+	@Override
+	public int findNumber(int max, Oracle oracle) {
+		
+		// Answer enum to hold the answer from the Oracle
+		Answer answer;
+		
+		// Answer enum that corresponds to a correct match
+		Answer answerCorrect = Answer.MATCH;
+		 
+		// Iterate to max
+		for(int i = 1; i < max; i++){
+			
+			// Ask the Oracle of i is correct
+			answer = oracle.test(i);
+			
+			// Check if answer equals answerCorrect
+			if(answer.equals(answerCorrect)){
+				
+				// Return i if so
+				return i;
+			}
+		}
+		
+		// Return -1 of nothing was found
+		return -1;
+	}
 }
