6203c8efbccf8efc7f241790fab59d32745d0c85
Sasamus <labus93@gmail.com>
2015-01-26 11:27:58 +0100

    Implemented class LogGuesser + some formatting

--- src/se/miun/dt015a/guesser/LinearGuesser.java
+++ src/se/miun/dt015a/guesser/LinearGuesser.java
@@ -3,9 +3,8 @@ package se.miun.dt015a.guesser;
 import se.miun.dt015a.guesser.Oracle.Answer;
 
 /**
- * Implements a linear guessing algorithm.
- * Complete this implementation so that findNumber returns the secret number
- * in time O(n).
+ * Implements a linear guessing algorithm. Complete this implementation so that
+ * findNumber returns the secret number in time O(n).
  *
  * If you want to be precise, then LinearGuesser should run in time Θ(log n),
  * but if you don't know the difference between Ο, Θ, and Ω, then ignore this
@@ -18,27 +17,24 @@ public class LinearGuesser implements Guesser {
 
 	@Override
 	public int findNumber(int max, Oracle oracle) {
-		
+
 		// Answer enum to hold the answer from the Oracle
 		Answer answer;
-		
-		// Answer enum that corresponds to a correct match
-		Answer answerCorrect = Answer.MATCH;
-		 
+
 		// Iterate to max
-		for(int i = 1; i < max; i++){
-			
+		for (int i = 1; i < max; i++) {
+
 			// Ask the Oracle of i is correct
 			answer = oracle.test(i);
-			
-			// Check if answer equals answerCorrect
-			if(answer.equals(answerCorrect)){
-				
+
+			// Check if answer is MATCH
+			if (answer.equals(Answer.MATCH)) {
+
 				// Return i if so
 				return i;
 			}
 		}
-		
+
 		// Return -1 of nothing was found
 		return -1;
 	}
--- src/se/miun/dt015a/guesser/LogGuesser.java
+++ src/se/miun/dt015a/guesser/LogGuesser.java
@@ -1,17 +1,64 @@
 package se.miun.dt015a.guesser;
 
+import java.util.Random;
+
+import se.miun.dt015a.guesser.Oracle.Answer;
+
 /**
- * Implements a logarithmic guessing algorithm.
- * Complete this implementation so that findNumber returns the secret number
- * in time Ο(log n).
+ * Implements a logarithmic guessing algorithm. Complete this implementation so
+ * that findNumber returns the secret number in time Ο(log n).
  *
  * @author Christoffer Fink
  */
 public class LogGuesser implements Guesser {
-  // Do not add any instance variables or constructors.
+	// Do not add any instance variables or constructors.
 
-  @Override
-  public int findNumber(int max, Oracle oracle) {
-    return -1; // Your code here.
-  }
+	@Override
+	public int findNumber(int max, Oracle oracle) {
+
+		// Answer enum to hold the answer from the Oracle
+		Answer answer;
+
+		// Holds the huess
+		int guess;
+
+		// Used to get a random number
+		Random random = new Random();
+
+		// Holds the upper limit
+		int upperLimit = max;
+
+		// Holds the lower limit
+		int lowerLimit = 1;
+
+		// Goes forever
+		while (1 < 2) {
+
+			// Get a guess in the range left to guess in
+			guess = random.nextInt(upperLimit) + lowerLimit;
+
+			// Ask the Oracle of i is correct
+			answer = oracle.test(guess);
+
+			// Check answer and act accordingly
+			if (answer.equals(Answer.TOO_HIGH)) {
+
+				// Set upperLimit to guess - 1, since the limit is inclusive
+				upperLimit = guess - 1;
+			} else if (answer.equals(Answer.TOO_LOW)) {
+
+				// Set lowerLimitLimit to guess + 1, since the limit is
+				// inclusive
+				lowerLimit = guess + 1;
+			} else if (answer.equals(Answer.MATCH)) {
+
+				// Return i if so
+				return guess;
+			} else {
+
+				// Return -1 of nothing was found
+				return -1;
+			}
+		}
+	}
 }
